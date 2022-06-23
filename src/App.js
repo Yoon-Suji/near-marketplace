03d32b053f9ccb6b808e7053efa47e9688b92630
Yoon-Suji <jidl88@ewhain.net>
2022-06-23 12:04:14 +0900

    finish the dapp

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useEffect, useCallback, useState } from "react";
 import { Container, Nav } from "react-bootstrap";
 import { login, logout as destroy, accountBalance } from "./utils/near";
 import Wallet from "./components/Wallet";
-// import { Notification } from "./components/utils/Notifications";
-// import Products from "./components/marketplace/Products";
+import { Notification } from "./components/utils/Notifications";
+import Products from "./components/marketplace/Products";
 import Cover from "./components/utils/Cover";
 import coverImg from "./assets/img/sandwich.jpg";
 import "./App.css";
@@ -23,7 +23,7 @@ function App() {
   
   return (
     <>
-      {/* <Notification /> */}
+      <Notification />
       {account.accountId ? (
         <Container fluid="md">
           <Nav className="justify-content-end pt-3 pb-5">
@@ -36,7 +36,7 @@ function App() {
               />
             </Nav.Item>
           </Nav>
-          <main>{/* <Products /> */}</main>
+          <main><Products /></main>
         </Container>
       ) : (
         <Cover name="Street Food" login={login} coverImg={coverImg} />
